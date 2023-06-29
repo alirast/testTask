c9b7506fcece34e9fafc51890cd42cf33ac41a2b
alirast <xdabnormal@gmail.com>
2023-06-29 10:34:31 +0300

    added coding keys for the models

--- testTask1/Models/DishModel.swift
+++ testTask1/Models/DishModel.swift
@@ -12,5 +12,13 @@ struct Dish: Decodable {
     let name: String
     let price: Int
     let description: String
-    let image_url: String
+    let imageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case price = "price"
+        case description = "description"
+        case imageURL = "image_url"
+    }
 }
